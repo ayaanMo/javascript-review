6a900f45203d7e5ffffeb9288f00a80ad0de66f3
ayaanMo <464958073@qq.com>
2021-11-18 14:15:01 +0800

    取消测试

--- 1.数据类型判断/index.js
+++ 1.数据类型判断/index.js
@@ -1,6 +1,5 @@
 // 数据类型判断
 // 所有类在继承Object的时候，改写了toString()方法；只有Object原型上的方法可以输出数据类型
-// 测试一下
 function typeOf(obj) {
     console.log(Object.prototype.toString.call(obj)); // [object Array]
     let res = Object.prototype.toString.call(obj).split(' ')[1];
