a6bcc94931415399fabc6d0564e1606d2a97b3e8
ayaan_90 <464958073@qq.com>
2022-08-09 15:18:44 +0800

    增加事件循环

--- README.md
+++
@@ -1,2 +0,0 @@
-# javascript-review
-js题目整理
--- test.js
+++ test.js
@@ -1,39 +1,6 @@
-// 数据类型判断
-console.log('----数据类型判断-----');
-function typeOf(obj) {
-    let result = Object.prototype.toString.call(obj).split(' ')[1];
-    result = result.substring(0, result.length - 1).toLowerCase();
-    return result;
-}
-console.log(typeOf(123));
-console.log(typeOf('123'));
-// 组合继承
-console.log('----组合继承-----');
-function Animal(params = {}) {
-    this.name = params['name'];
-    this.age = params['age'];
-    this.colors = ['white', 'black'];
-}
-Animal.prototype.getName = function () {
-    return this.name;
-};
-function Dog(params) {
-    Animal.call(this, params);
-    this.sex = params['sex'];
-}
-Dog.prototype = new Animal();
-Dog.prototype.constructor = Dog;
-let dog1 = new Dog({ name: 'James', age: 3, sex: '公' });
-let dog2 = new Dog({ name: 'Lucy', age: 4, sex: '母' });
-dog1.colors.push('red');
-console.log(dog1);
-console.log(dog2);
-// 数组去重
-console.log('----数组去重-----');
-function unique(arr) {
-    let result = arr.filter((item, index) => {
-        return arr.indexOf(item) === index;
-    });
-    return result;
-}
-console.log(unique([1, 2, 3, 4, 4, 5, 5, 6, 6, 6, 7, 7, 7]));
+let arrM = ['2022-05月', '2022-09月', '2022-02月', '2022-07月', '2022-03月'];
+let arrY = ['2024', '2021', '2022', '2020'];
+let arrW = ['2022-26周', '2022-26周', '2022-27周', '2022-23周', '2022-21周'];
+console.log(arrM.sort());
+console.log(arrY.sort());
+console.log(arrW.sort());
