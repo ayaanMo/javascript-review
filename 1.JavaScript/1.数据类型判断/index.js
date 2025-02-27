a4eaa5e750dc3d8fc184eb19d28db4a1aa522e1a
ayaanMo <464958073@qq.com>
2025-02-27 17:23:09 +0800

    你的提交信息页面更改

--- 1.JavaScript/1.数据类型判断/index.js
+++ 1.JavaScript/1.数据类型判断/index.js
@@ -1,28 +1,29 @@
 // 数据类型判断
 // 所有类在继承Object的时候，改写了toString()方法；只有Object原型上的方法可以输出数据类型
 function typeOf(obj) {
-    console.log(Object.prototype.toString.call(obj)); // [object Array]
-    let res = Object.prototype.toString.call(obj).split(' ')[1];
-    console.log(res); // Array]
+  console.log(Object.prototype.toString.call(obj)); // [object Array]
+  let res = Object.prototype.toString.call(obj).split(" ")[1];
+  console.log(res); // Array]
 
-    res = res.substring(0, res.length - 1).toLowerCase();
-    return res;
+  res = res.substring(0, res.length - 1).toLowerCase();
+  return res;
 }
-console.log(typeOf(['a']));
-console.log('--------------------');
+console.log(typeOf(["a"]));
+console.log("--------------------");
 console.log(typeOf({ a: 1 }));
-console.log('--------------------');
+console.log("--------------------");
 console.log(typeOf(null));
-console.log('--------------------');
+console.log("--------------------");
 console.log(typeOf(undefined));
-console.log('--------------------');
+console.log("--------------------");
 console.log(typeOf(1));
-console.log('--------------------');
-console.log(typeOf('1'));
-console.log('--------------------');
+console.log("--------------------");
+console.log(typeOf("1"));
+console.log("--------------------");
 console.log(typeOf(false));
 {
-    let str = 'abcdefghijk-lmnopqrstuvwxyz';
-    let strs = str.substring(0, str.length - 1);
-    console.log(strs);
+  let str = "abcdefghijk-lmnopqrstuvwxyz";
+  let strs = str.substring(0, str.length - 1);
+  console.log(strs);
 }
+console.log("abcdefg".substring(0, "abcdefg".length));
