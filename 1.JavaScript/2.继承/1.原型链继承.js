78d4e23a2902ae841e8895cb2f715215417c6fbe
ayaanMo <464958073@qq.com>
2025-02-28 16:34:34 +0800

    提交

--- 1.JavaScript/2.继承/1.原型链继承.js
+++ 1.JavaScript/2.继承/1.原型链继承.js
@@ -6,17 +6,17 @@
 // 缺陷4：来自原型对象的所有属性被所有实例共享；
 // 缺陷5：创建子类实例时，无法向父类构造函数传参。
 function Animal() {
-    this.colors = ['black', 'white'];
+  this.colors = ["black", "white"];
 }
 // prototype是函数独有的，prototype就是一个对象，prototype里面有两个属性：__proto__和constructor
 // 原型可以用来共享方法
 // 原型中的this指向实例
 Animal.prototype.getColor = function () {
-    return this.colors;
+  return this.colors;
 };
 function Dog() {}
 Dog.prototype = new Animal();
 let dog1 = new Dog();
-dog1.colors.push('brown');
+dog1.colors.push("brown");
 let dog2 = new Dog();
 console.log(dog2.colors); // ['black', 'white', 'brown']
